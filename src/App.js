592cbe07512688a4b47f26ee4fec3102c2680f8d
mdmaruf43 <marufahmedcse76@gmail.com>
2020-07-07 13:14:20 +0600

    redux is awesome

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { Provider } from 'react-redux'
 import './App.css';
 import CakeContainer from './components/CakeContainer';
+import store from './redux/store';
+import HooksCakeContainer from './components/HooksCakeContainer';
 
 function App() {
   return (
-    <div className="App">
-      <CakeContainer />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <HooksCakeContainer />
+        <CakeContainer />
+      </div>
+    </Provider>
+    
   );
 }
 
