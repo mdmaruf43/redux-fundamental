11b808b5ef416f6b69dbe5bae0d28cfe73824e9b
mdmaruf43 <marufahmedcse76@gmail.com>
2020-07-07 12:03:20 +0600

    added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import CakeContainer from './components/CakeContainer';
 
 function App() {
   return (
     <div className="App">
-
+      <CakeContainer />
     </div>
   );
 }
